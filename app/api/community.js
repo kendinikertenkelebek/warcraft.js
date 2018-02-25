cafc3160fd06641f25878ab9200e1e2a41bded03
Yankı Küçük <yankikucuk@Yank-iMac.local>
2018-02-25 19:28:31 +0300

    Added AuctionHouse

--- app/api/community.js
+++ app/api/community.js
@@ -2,11 +2,13 @@
 
 const Guild = require('./subs/community/guild/guild');
 const Item = require('./subs/community/item/item');
+const Auction = require('./subs/community/auction/auction');
 
 class Community {
   constructor(options) {
     this.guild = new Guild(options);
     this.item = new Item(options);
+    this.auction = new Auction(options);
   }
 }
 
---
+++ app/api/subs/community/auction/auction.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const request = require('request-promise');
+const endpoints = require('../../../../../util/endpoints');
+const { Urls } = require('../../../../../util/constants');
+
+class Auction {
+  constructor(options) {
+    this.options = options;
+  }
+
+  async get(realmName) {
+    const apiURL = this._makeURL(this.options, realmName || null);
+    if (apiURL && typeof apiURL !== 'undefined') {
+      try {
+        const response = await request(apiURL);
+        return response;
+      } catch (err) {
+        const fail = { fail: true };
+        return fail;
+      }
+    }
+    return null;
+  }
+
+
+  _makeURL(options, realmName) {
+    const endpoint = endpoints.getEndpoint(options.region, options.locale);
+    const link = Urls.auction
+      .replace(/\{hostname\}/, endpoint.hostname)
+      .replace(/\{realm\}/, realmName || options.realm)
+      .replace(/\{locale\}/, endpoint.locale)
+      .replace(/\{apiKey\}/, options.apiKey);
+    const reqURL = { method: 'GET', uri: link, json: true };
+    return reqURL;
+  }
+}
+
+module.exports = Auction;
