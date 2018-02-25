cdd4bd33389d2ebd2149eb958a255f00633a2828
Yankı Küçük <yankikucuk@Yank-iMac.local>
2018-02-25 19:37:40 +0300

    Added AuctionHouse

--- util/constants.js
+++ util/constants.js
@@ -11,6 +11,7 @@ exports.DefaultOptions = {
 exports.Urls = {
   guild: '{hostname}/wow/guild/{realm}/{guild}?fields={field}&locale={locale}&apikey={apiKey}',
   item: '{hostname}/wow/item/{itemId}?locale={locale}&apikey={apiKey}',
+  auction: '{hostname}/wow/auction/data/{realm}?locale={locale}&apikey={apiKey}',
   realm: '{hostname}/data/wow/realm/{slug}?namespace=dynamic-{region}&locale={locale}&access_token={access_token}',
   connectedRealm: '{hostname}/data/wow/connected-realm/{realmId}/mythic-leaderboard/?namespace=dynamic-{region}&locale={locale}&access_token={access_token}', // eslint-disable-line max-len
   dungeon: '{dungeonLink}&locale={locale}&access_token={access_token}'
@@ -20,4 +21,4 @@ exports.Jpgs = {
   charThumbnail: 'https://render-{continent}.worldofwarcraft.com/character/{api-url}.jpg',
   npcThumbnail: 'http://media.blizzard.com/wow/renders/npcs/zoom/creature{creatureId}.jpg',
   icons: 'http://media.blizzard.com/wow/icons/{size}/{icon_name}.jpg' // Size: 18-36-56
-}
+};
